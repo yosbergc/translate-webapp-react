bf112cefd3d869cb491a2cedea28fdf1b6ef1247
Yosber Guerrero <yosbergc@gmail.com>
2024-04-16 19:16:53 -0400

    Added background to the body, added logo

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Logo from '../../svg/logo.svg'
+function App() {
+  return (
+    <section className='app-container'>
+      <header>
+        <img src={Logo} alt='Logo'></img>
+      </header>
+    </section>
+  );
+}
+
+export default App;
